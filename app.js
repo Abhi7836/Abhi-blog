5bc7e6a8981d86fe5f0e3384accc88246b6a2626
Sugali Abhishek <sugali.alnaikk.ece17@itbhu.ac.in>
2022-01-23 13:46:58 +0530

    make it better

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-//mongoose.connect('mongodb://localhost:27017/testblog', {useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.connect(process.env.MONGO_URL,{useNewUrlParser: true, useUnifiedTopology: true});
 
+mongoose.connect(process.env.MONGO_URL,{useNewUrlParser: true, useUnifiedTopology: true});
+console.log("connected to mongoose")
 
 const blogSchema = {title: String, desc:String};
 const Blog = mongoose.model("Blog", blogSchema);
@@ -54,5 +54,5 @@ app.post("/contact",function(req,res){
 let port = process.env.PORT;
 if (port == null || port == "") {port = 3001;}
 app.listen(port, function() {
-  console.log("Server started on port 3000");
+  console.log("Server started on port 3001");
 });
